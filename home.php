c10e01f82d1c6d14a8d077cf5f4a532a5770e872
Nguyễn Phú Đức <nguyenphuduc62001@gmail.com>
2024-08-15 14:17:50 +0700

    add home page

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo '<p style="text-align: center; color: green;">Xin chào TMU</p>';
+echo '<button style="background-color:red;">click</button>';
+echo '<a target="_blank" href ="https://www.youtube.com/">Youtube</a> '
+?>
+    
+</body>
+</html>
